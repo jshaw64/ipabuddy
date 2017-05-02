e789b3dc33e8a73103083d9f29e7f5289496d503
Joel Shaw <Joel_Shaw@colpal.com>
2017-05-02 02:55:53 -0400

    Change install dir to support remote install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 (
-install_dir=$(pwd)
+install_dir="$(dirname "${BASH_SOURCE[0]}")"
 [ ! -d /usr/local/bin ] && mkdir /usr/local/bin
 cd /usr/local/bin
 ln -s ${install_dir}/ipabuddy.sh ipabuddy
